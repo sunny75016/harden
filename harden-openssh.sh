b7c16672a7cef6f2dcdc7c4988125fd40b508512
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-04-22 06:45:30 +0100

    Update harden-openssh.sh

--- harden-openssh.sh
+++ harden-openssh.sh
@@ -1,33 +1,20 @@
 #!/bin/bash
 echo "Updating OpenSSH to version 8.2 from https://www.openssh.com/"
-read -p "Press [Enter] key to continue"
 echo "Installing required package dependencies..."
-read -p "Press [Enter] key to continue"
-sudo apt install build-essential zlib1g-dev libssl-dev libpam0g-dev libselinux1-dev -y
-read -p "Press [Enter] key to continue"
-mkdir /var/lib/sshd 
-read -p "Press [Enter] key to continue"
-chmod -R 700 /var/lib/sshd/ 
-read -p "Press [Enter] key to continue"
-useradd -r -U -d /var/lib/sshd/ -c "sshd privsep" -s /bin/false sshd
-read -p "Press [Enter] key to continue"
-echo "Ignore the error above that user 'sshd' already exists."
-read -p "Press [Enter] key to continue"
+sudo apt install build-essential zlib1g-dev libssl-dev libpam0g-dev libselinux1-dev -y > /dev/null
+mkdir /var/lib/sshd > /dev/null
+chmod -R 700 /var/lib/sshd/ > /dev/null
+useradd -r -U -d /var/lib/sshd/ -c "sshd privsep" -s /bin/false sshd > /dev/null
+echo "Ignore if you see an error message that user 'sshd' already exists."
 echo "Downloading OpenSSH..."
-read -p "Press [Enter] key to continue"
-wget -c --no-check-certificate https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-8.2p1.tar.gz  
-read -p "Press [Enter] key to continue"
-tar -xzf openssh-8.2p1.tar.gz 
-cd openssh-8.2p1/
-read -p "Press [Enter] key to continue"
-./configure --with-md5-passwords --with-selinux --with-privsep-path=/var/lib/sshd/ --sysconfdir=/etc/ssh
-read -p "Press [Enter] key to continue"
-echo "Installing OpenSSH..."
-read -p "Press [Enter] key to continue"
-make
-make install
-echo "MANUALLY EDIT /etc/ssh/sshd_config and change to usepam=no"
+wget -c --no-check-certificate https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-8.2p1.tar.gz  > /dev/null
+tar -xzf openssh-8.2p1.tar.gz > /dev/null
+cd openssh-8.2p1/ > /dev/null
+./configure --with-md5-passwords --with-selinux --with-privsep-path=/var/lib/sshd/ --sysconfdir=/etc/ssh > /dev/null
+echo "Installing OpenSSH...(have patience!)"
+make > /dev/null
+make install > /dev/null
+echo "MANUALLY EDIT /etc/ssh/sshd_config and change to usepam=no (unless you need it)"
 echo "OpenSSH has been updated. The result below may still show older ssh."
-read -p "Press [Enter] key to continue"
 ssh -V
 echo "Reboot at your convenience and run <ssh -V> to see the new version."
