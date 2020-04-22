9da15d2fed007058c3eb193752c50724c8d0ae3a
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-04-22 07:06:15 +0100

    Update harden-kernel.sh

--- harden-kernel.sh
+++ harden-kernel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Updating to Linux Kernel v.5.6.6 mainline build for AMD64 from https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.6/"
-echo "Checking updates..."
-apt update > /dev/null
-echo "Installing upgrades..."
-apt upgrade -y > /dev/null
+echo "Checking updates...(this should be quick)"
+apt-get update > /dev/null
+echo "Installing upgrades...(have patience!)"
+apt-get upgrade -y > /dev/null
 cd /tmp > /dev/null
-echo "Downloading kernel files..."
+echo "Downloading kernel files...(this should be quick)"
 wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.6/linux-headers-5.6.6-050606_5.6.6-050606.202004210831_all.deb > /dev/null
 wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.6/linux-headers-5.6.6-050606-generic_5.6.6-050606.202004210831_amd64.deb > /dev/null
 wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.6/linux-image-unsigned-5.6.6-050606-generic_5.6.6-050606.202004210831_amd64.deb > /dev/null
