32dbe535208640035a4576e0641fcce445ce99dc
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-04-22 15:09:23 +0100

    Update harden-all.sh

--- harden-all.sh
+++ harden-all.sh
@@ -3,5 +3,6 @@ chmod +x *.sh
 ./harden-kernel.sh
 ./harden-openssl.sh
 ./harden-openssh.sh
+./harden-ppa.sh
 ./harden-lynis.sh
 echo "Thanks for using harden-all.sh from https://github.com/sunny75016/harden.git"
