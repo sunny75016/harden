0f55a5454b308895eed17d3715dd1e5b5ac1d4bc
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-04-22 13:41:26 +0100

    Create harden-ppa.sh

---
+++ harden-ppa.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo add-apt-repository ppa:ondrej/apache2
+sudo add-apt-repository ppa:ondrej/nginx-mainline
+sudo add-apt-repository ppa:ondrej/php
+echo "Enter 1 for Bionic Beaver, 2 for Focal Fossa"
+read n
+if [ $n -eq 1 ];
+then
+echo "Adding MariaDB 10.4 stable repositary for Ubuntu 18.04"
+sudo apt-get install software-properties-common
+sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://mirrors.ukfast.co.uk/sites/mariadb/repo/10.4/ubuntu bionic main'
+fi
+if [ $n -eq 2 ];
+then
+echo "Adding MariaDB 10.5 beta repositary for Ubuntu 20.04"
+sudo apt-get install software-properties-common
+sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+sudo add-apt-repository 'deb [arch=amd64] http://mirrors.ukfast.co.uk/sites/mariadb/repo/10.5/ubuntu focal main'
+fi
