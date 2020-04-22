8a432061e114ff64b2bea748464b30db2da11051
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-04-22 07:10:35 +0100

    Update harden-kernel.sh

--- harden-kernel.sh
+++ harden-kernel.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 echo "Updating to Linux Kernel v.5.6.6 mainline build for AMD64 from https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.6/"
-echo "Checking updates...(this should be quick)"
-apt-get update > /dev/null
+apt-get update 
 echo "Installing upgrades...(have patience!)"
-apt-get upgrade -y > /dev/null
-cd /tmp > /dev/null
+apt-get upgrade -y 
+cd /tmp
 echo "Downloading kernel files...(this should be quick)"
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.6/linux-headers-5.6.6-050606_5.6.6-050606.202004210831_all.deb > /dev/null
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.6/linux-headers-5.6.6-050606-generic_5.6.6-050606.202004210831_amd64.deb > /dev/null
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.6/linux-image-unsigned-5.6.6-050606-generic_5.6.6-050606.202004210831_amd64.deb > /dev/null
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.6/linux-modules-5.6.6-050606-generic_5.6.6-050606.202004210831_amd64.deb > /dev/null
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.6/linux-headers-5.6.6-050606_5.6.6-050606.202004210831_all.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.6/linux-headers-5.6.6-050606-generic_5.6.6-050606.202004210831_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.6/linux-image-unsigned-5.6.6-050606-generic_5.6.6-050606.202004210831_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.6/linux-modules-5.6.6-050606-generic_5.6.6-050606.202004210831_amd64.deb
 echo "Installing kernel...(have patience!)"
-dpkg -i *.deb > /dev/null
-echo "Kernel has been updated. The result below may still show older kernel."
-uname -a
+dpkg -i *.deb 
 echo "Reboot at your convenience and run <uname -a> to see the new kernel."
