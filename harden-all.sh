00a9a576572525f5a470b94c4be69ca89518c12f
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-04-24 13:29:09 +0100

    Update harden-all.sh

--- harden-all.sh
+++ harden-all.sh
@@ -4,5 +4,19 @@ chmod +x *.sh
 ./harden-openssl.sh
 ./harden-openssh.sh
 ./harden-ppa.sh
+./harden-profile.sh
+./harden-chmod.sh
+./harden-proc.sh
+mkdir /backup/
+cp /etc/issue /backup/
+cp issue /etc/
+cp /etc/issue.net /backup/
+cp issue.net /etc/
+cp /etc/login.defs /backup/
+cp login.defs /etc/
+cp /etc/ssh/sshd_config /backup/
+cp sshd_config /etc/ssh/
+cp local.conf /etc/sysctl.d/
+cp coredump.conf /etc/systemd/
 ./harden-lynis.sh
 echo "Thanks for using harden-all.sh from https://github.com/sunny75016/harden.git"
