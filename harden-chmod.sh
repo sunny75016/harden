681cea90bfdd4c903d7ec70fb317378f73f806fc
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-04-24 09:29:10 +0100

    Create harden-chmod.sh

---
+++ harden-chmod.sh
@@ -0,0 +1,14 @@
+!/bin/bash
+chmod 600 /boot/grub/grub.cfg
+chmod 600 /etc/at.deny
+chmod 600 /etc/crontab 
+chmod 600 /etc/ssh/sshd_config
+chmod 700 /etc/cron.d
+chmod 700 /etc/cron.daily
+chmod 700 /etc/cron.hourly
+chmod 700 /etc/cron.weekly
+chmod 700 /etc/cron.monthly
+chmod o-x /usr/bin/x86_64-linux-gnu-as
+chmod o-x /usr/bin/x86_64-linux-gnu-gcc-9
+
+
