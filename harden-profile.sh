f3b52eb872ca599c282c0dc40f55f0aeb8ec59bc
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-04-24 15:44:54 +0100

    Update harden-profile.sh

--- harden-profile.sh
+++ harden-profile.sh
@@ -2,6 +2,6 @@
 echo "ulimit -c 0" >> /etc/profile
 echo "* hard core 0" >> /etc/profile
 echo "* soft core 0" >> /etc/profile
-TMOUT=3600
-readonly TMOUT
-export TMOUT
+echo "TMOUT=3600" >> /etc/profile
+echo "readonly TMOUT" >> /etc/profile
+echo "export TMOUT" >> /etc/profile
