5fe103a51c3fcf91814f0d6c0fbfc8de31a3b0bb
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-04-24 15:59:25 +0100

    Update harden-all.sh

--- harden-all.sh
+++ harden-all.sh
@@ -14,8 +14,8 @@ cp issue.net /etc/
 cp /etc/login.defs /backup/
 cp login.defs /etc/
 cp /etc/ssh/sshd_config /backup/
-cp sshd_config /etc/ssh/
-cp local.conf /etc/sysctl.d/
-cp coredump.conf /etc/systemd/
-./harden-lynis.sh
+#cp sshd_config /etc/ssh/
+#cp local.conf /etc/sysctl.d/
+#cp coredump.conf /etc/systemd/
+#./harden-lynis.sh
 echo "Thanks for using harden-all.sh from https://github.com/sunny75016/harden.git"
