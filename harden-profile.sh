60d26f1e089df51a90b75d72fd273c2ad2dcfcd3
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-04-24 17:05:22 +0100

    Update harden-profile.sh

--- harden-profile.sh
+++ harden-profile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "ulimit -c 0" >> /etc/profile
-echo "* hard core 0" >> /etc/profile
-echo "* soft core 0" >> /etc/profile
+echo "ulimit -c 0 > /dev/null 2>&1" > /etc/profile.d/disable-coredumps.sh
 echo "TMOUT=3600" >> /etc/profile
 echo "readonly TMOUT" >> /etc/profile
 echo "export TMOUT" >> /etc/profile
+echo "* hard core 0" >> /etc/security/limits.conf
+echo "* soft core 0" >> /etc/security/limits.conf
