a5e6d3ce981891430fa6bd406e4a806943c8098e
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-04-25 10:12:41 +0100

    Update harden-openssl.sh

--- harden-openssl.sh
+++ harden-openssl.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 echo "Updating to OpenSSL v.1.1.1g from https://www.openssl.org/source/openssl-1.1.1g.tar.gz"
 echo "Installing dependencies..."
-apt update
-apt install make -y 
-apt install gcc -y 
-cd /usr/src  echo "Downloading OpenSSL..."
-sudo wget https://www.openssl.org/source/openssl-1.1.1g.tar.gz --no-check-certificate 
-sudo tar -zxf openssl-1.1.1g.tar.gz 
-cd openssl-1.1.1g 
-./config 
+apt-get update > /dev/null
+apt-get install make -y > /dev/null
+apt-get install gcc -y > /dev/null
+cd /usr/src  
+echo "Downloading OpenSSL..."
+sudo wget https://www.openssl.org/source/openssl-1.1.1g.tar.gz --no-check-certificate > /dev/null
+sudo tar -zxf openssl-1.1.1g.tar.gz > /dev/null
+cd openssl-1.1.1g > /dev/null
+./config > /dev/null
 echo "Installing OpenSSL...(have patience!)"
-make 
+echo "Running make"
+make > /dev/null
+echo "Running make test"
 make test 
-make install 
+echo "Running make install"
+make install > /dev/null
 echo "Completing the links"
 echo "/usr/local/lib64" > /etc/ld.so.conf.d/openssl.conf
-ldconfig 
-echo "OpenSSL has been updated. The result below may not be accurate."
-openssl version
+ldconfig > /dev/null
 echo "Reboot at your convenience and run <openssl version> to verify the update."
