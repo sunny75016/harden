74dcafb90bd43ed49cdb102d766fe58cfcb12c6f
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-04-25 08:00:09 +0100

    Update harden-filesystems.sh

--- harden-filesystems.sh
+++ harden-filesystems.sh
@@ -6,3 +6,7 @@ echo "install hfspus /bin/true" >> /etc/modprobe.d/blacklist.conf
 echo "install jffs2 /bin/true" >> /etc/modprobe.d/blacklist.conf
 echo "install udf /bin/true" >> /etc/modprobe.d/blacklist.conf
 echo "blacklist usb-storage" >> /etc/modprobe.d/blacklist.conf 
+echo "install dccp /bin/true" >> /etc/modprobe.d/blacklist.conf
+echo "install sctp /bin/true" >> /etc/modprobe.d/blacklist.conf
+echo "install rds /bin/true" >> /etc/modprobe.d/blacklist.conf
+echo "install tipc /bin/true" >> /etc/modprobe.d/blacklist.conf
