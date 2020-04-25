b083b3e30299b07e4cee77559e009779345672c5
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-04-25 21:16:53 +0100

    Update harden-install-programs.sh

--- harden-install-programs.sh
+++ harden-install-programs.sh
@@ -12,6 +12,8 @@ echo "Correcting rkhunter.conf"
 sed -i 's/UPDATE_MIRRORS=0/UPDATE_MIRRORS=1/' /etc/rkhunter.conf > /dev/null
 sed -i 's/MIRRORS_MODE=1/MIRRORS_MODE=0/' /etc/rkhunter.conf > /dev/null
 sed -i 's/WEB_CMD=\"\/bin\/false\"/WEB_CMD=\"\"/' /etc/rkhunter.conf > /dev/null
+sed -i 's/\#ALLOW_SSH_ROOT_USER=no/ALLOW_SSH_ROOT_USER=without-password/ > /dev/null
+sed -i 's/ALLOW_SSH_PROT_V1=2/ALLOW_SSH_PROT_V1=0/ > /dev/null
 echo "Hardening default Postfix installation from rkhunter"
 sed -i 's/smtpd_banner.*/smtpd_banner \= \$myhostname ESMTP/' /etc/postfix/main.cf > /dev/null
 postconf -e disable_vrfy_command=yes > /dev/null
