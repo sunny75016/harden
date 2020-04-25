ab960e19c7ec57421f0a0cd3bfa824eee64ec90c
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-04-25 10:24:25 +0100

    Update harden-ppa.sh

--- harden-ppa.sh
+++ harden-ppa.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo add-apt-repository ppa:ondrej/apache2
+echo "Hardening PPAs for apache2, nginx, php and MariaDB"
+echo "Please follow instructions on the screen"
+sudo add-apt-repository ppa:ondrej/apache2 
 sudo add-apt-repository ppa:ondrej/nginx-mainline
 sudo add-apt-repository ppa:ondrej/php
 echo "Enter 1 for Bionic Beaver, 2 for Focal Fossa"
@@ -18,7 +20,8 @@ sudo apt-get install software-properties-common
 sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
 sudo add-apt-repository 'deb [arch=amd64] http://mirrors.ukfast.co.uk/sites/mariadb/repo/10.5/ubuntu focal main'
 fi
-apt update
-apt upgrade -y
-apt autoremove -y
-apt clean
+echo "Updating available packages and cleaning"
+apt-get update > /dev/null
+apt-get upgrade -y > /dev/null
+apt-get autoremove -y > /dev/null
+apt-get clean > /dev/null
