102499bc4584ef3376191b5bec5afc7f50a70af8
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-04-25 10:25:42 +0100

    Update harden-kernel.sh

--- harden-kernel.sh
+++ harden-kernel.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "Updating to Linux Kernel v.5.6.7 mainline build for AMD64 from https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.7/"
-apt-get update 
+apt-get update > /dev/null
 echo "Installing upgrades...(have patience!)"
-apt-get upgrade -y 
-cd /tmp
+apt-get upgrade -y > /dev/null
+cd /tmp > /dev/null
 echo "Downloading kernel files...(this should be quick)"
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.7/linux-headers-5.6.7-050607_5.6.7-050607.202004230933_all.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.7/linux-headers-5.6.7-050607-generic_5.6.7-050607.202004230933_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.7/linux-image-unsigned-5.6.7-050607-generic_5.6.7-050607.202004230933_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.7/linux-modules-5.6.7-050607-generic_5.6.7-050607.202004230933_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.7/linux-headers-5.6.7-050607_5.6.7-050607.202004230933_all.deb > /dev/null
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.7/linux-headers-5.6.7-050607-generic_5.6.7-050607.202004230933_amd64.deb > /dev/null
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.7/linux-image-unsigned-5.6.7-050607-generic_5.6.7-050607.202004230933_amd64.deb > /dev/null
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.6.7/linux-modules-5.6.7-050607-generic_5.6.7-050607.202004230933_amd64.deb > /dev/null
 echo "Installing kernel...(have patience!)"
-dpkg -i *.deb 
+dpkg -i *.deb > /dev/null
 echo "Reboot at your convenience and run <uname -a> to see the new kernel."
