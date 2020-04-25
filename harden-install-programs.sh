90dff2717343f86e44f77ea5a4a6076691b06e0e
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-04-25 15:55:25 +0100

    Update harden-install-programs.sh

--- harden-install-programs.sh
+++ harden-install-programs.sh
@@ -13,7 +13,7 @@ sed -i 's/UPDATE_MIRRORS=0/UPDATE_MIRRORS=1/' /etc/rkhunter.conf > /dev/null
 sed -i 's/MIRRORS_MODE=1/MIRRORS_MODE=0/' /etc/rkhunter.conf > /dev/null
 sed -i 's/WEB_CMD=\"\/bin\/false\"/WEB_CMD=\"\"/' /etc/rkhunter.conf > /dev/null
 echo "Hardening default Postfix installation from rkhunter"
-sed -i 's/smtpd_banner.*/smtpd_banner \$myhostname ESMTP/' /etc/postfix/main.cf > /dev/null
+sed -i 's/smtpd_banner.*/smtpd_banner \= \$myhostname ESMTP/' /etc/postfix/main.cf > /dev/null
 postconf -e disable_vrfy_command=yes > /dev/null
 echo "Installing haveged for random numbers"
 apt-get install haveged -y
