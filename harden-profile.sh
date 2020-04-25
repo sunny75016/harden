2a6dd74fb5cffc1da3109c8a696f697999fba5a4
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-04-25 10:31:44 +0100

    Update harden-profile.sh

--- harden-profile.sh
+++ harden-profile.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Disabling kernel dumps and setting timeout to 1 hour"
 echo "ulimit -c 0 > /dev/null 2>&1" > /etc/profile.d/disable-coredumps.sh
 echo "TMOUT=3600" >> /etc/profile
 echo "readonly TMOUT" >> /etc/profile
@@ -7,4 +8,4 @@ echo "* hard core 0" >> /etc/security/limits.conf
 echo "* soft core 0" >> /etc/security/limits.conf
 echo "fs.suid_dumpable=0" >> /etc/sysctl.conf
 echo "kernel.kptr_restrict=2" >> /etc/sysctl.conf
-sysctl -p
+sysctl -p > /dev/null
