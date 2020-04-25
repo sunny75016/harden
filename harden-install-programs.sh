6eece533260d1a3b4815be3e9647f9009f295057
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-04-25 09:41:22 +0100

    Update harden-install-programs.sh

--- harden-install-programs.sh
+++ harden-install-programs.sh
@@ -3,3 +3,5 @@ apt-get install apt-show-versions -y
 apt-get install libpam-cracklib -y
 apt-get install debsums -y
 apt-get install rkhunter -y
+sed -i 's/smtpd_banner.*/smtpd_banner $myhostname ESMTP/' /etc/postfix/main.cf
+postconf -e disable_vrfy_command=yes
