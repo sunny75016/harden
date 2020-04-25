f713b56125b5b292a8a9155986536ea38206ae49
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-04-25 07:50:05 +0100

    Update harden-filesystems.sh

--- harden-filesystems.sh
+++ harden-filesystems.sh
@@ -5,4 +5,4 @@ echo "install freevxfs /bin/true" >> /etc/modprobe.d/blacklist.conf
 echo "install hfspus /bin/true" >> /etc/modprobe.d/blacklist.conf
 echo "install jffs2 /bin/true" >> /etc/modprobe.d/blacklist.conf
 echo "install udf /bin/true" >> /etc/modprobe.d/blacklist.conf
-echo "blacklist usb-storage" >> /etc/modproble.d/blacklist.conf 
+echo "blacklist usb-storage" >> /etc/modprobe.d/blacklist.conf 
