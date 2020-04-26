e4c839dd543dd69a2a2dcc7038057f9ac85d57d3
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-04-26 09:47:30 +0100

    Update harden-install-programs.sh

--- harden-install-programs.sh
+++ harden-install-programs.sh
@@ -2,10 +2,13 @@
 clear
 echo "Installing apt-show-versions"
 apt-get install apt-show-versions -y > /dev/null
+clear
 echo "Installing pam"
 apt-get install libpam-cracklib -y > /dev/null
+clear
 echo "Installing debsums"
 apt-get install debsums -y > /dev/null
+clear
 echo "Installing Rootkit Hunter"
 apt-get install rkhunter -y 
 echo "Correcting rkhunter.conf"
@@ -17,7 +20,17 @@ sed -i 's/ALLOW_SSH_PROT_V1=2/ALLOW_SSH_PROT_V1=0/' /etc/rkhunter.conf > /dev/nu
 echo "Hardening default Postfix installation from rkhunter"
 sed -i 's/smtpd_banner.*/smtpd_banner \= \$myhostname ESMTP/' /etc/postfix/main.cf > /dev/null
 postconf -e disable_vrfy_command=yes > /dev/null
+clear
 echo "Installing haveged for random numbers"
 apt-get install haveged -y
+clear
 echo "Installing ClamAV antivirus"
 sudo apt-get install clamav -y
+clear
+echo "Installing auditd with basic rules"
+sudo apt-get install auditd -y > /dev/null
+sudo systemctl start auditd
+sudo systemctl enable auditd
+echo "-w /etc/shadow -p wa -k shadow" >> /etc/audit/rules.d/audit.rules
+echo "-w /etc/passwd -p wa -k passwd" >> /etc/audit/rules.d/audit.rules
+sudo systemctl restart auditd
