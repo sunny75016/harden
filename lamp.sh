8e243ca23d38e6ea92a38cd04db3c5a6642e70a4
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-04-26 07:24:08 +0100

    Create lamp.sh

---
+++ lamp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+clear
+echo "Setting up LAMP stack, starting with apache2"
+apt-get update > /dev/null
+apt-get install apache2
+systemctl start apache2
+systemctl enable apache2
+clear
+echo "Setting up MariaDB server"
+apt-get install mariadb-server
+mysql_secure_installation
+clear
+echo "Setting up php 7.4"
+apt-get install php7.4 -y
+apt-get install libapache2-mod-php7.4 -y
+apt-get php7.4-mysql -y
+apt-get install phpmyadmin -y
