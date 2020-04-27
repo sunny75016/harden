cec1097d1ac19d20c995d2c67936f859d4fcda43
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-04-27 18:02:07 +0100

    Update harden-all.sh

--- harden-all.sh
+++ harden-all.sh
@@ -11,22 +11,8 @@
 ./harden-filesystem.sh
 ./harden-install-programs.sh
 ./harden-stig.sh
-echo "Creating the directory /backup/ for files overwritten" 
-mkdir /backup/ > /dev/null
-cp /etc/issue /backup/ > /dev/null
-cp issue /etc/ > /dev/null
-cp /etc/issue.net /backup/ > /dev/null
-cp issue.net /etc/ > /dev/null
-cp /etc/login.defs /backup/ > /dev/null
-cp login.defs /etc/ > /dev/null
-cp /etc/ssh/sshd_config /backup/ > /dev/null
-chmod +r /etc/ssh/sshd_config > /dev/null
-cp sshd_config /etc/ssh/ > /dev/null
-chmod 600 /etc/ssh/sshd_config > /dev/null
-cp local.conf /etc/sysctl.d/ > /dev/null
-cp coredump.conf /etc/systemd/ > /dev/null
-sysctl -p > /dev/null
-#./harden-lynis.sh
+./harden-model-files.sh
+./harden-lynis.sh
 clear
 echo "Thanks for using harden-all.sh from https://github.com/sunny75016/harden.git"
 cd /home/harden > /dev/null
