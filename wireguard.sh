0bb6d8f90f59e77773d9deef9882ab22230b2f01
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-05-02 10:47:43 +0100

    Create wireguard.sh

---
+++ wireguard.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+clear
+echo "Installing Wireguard from Ubuntu Focal repository"
+sudo apt-get install wireguard -y
+echo "Creating wg keys"
+cd ~
+umask 077
+wg genkey | tee privatekey | wg pubkey > publickey
+ls -al
+touch /etc/wireguard/wg0.conf
+echo "[Interface]" >> /etc/wireguard/wg0.conf
+echo "PrivateKey = $(cat ~/privatekey)" >> /etc/wireguard/wg0.conf
+echo "Address = 10.0.0.1/24, fd86:ea04:1115::1/64" >> /etc/wireguard/wg0.conf
+echo "ListenPort = 51820" >> /etc/wireguard/wg0.conf
+echo "PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE; ip6tables -A FORWARD -i wg0 -j ACCEPT; ip6tables -t nat -A POSTROUTING -o eth0 -j MASQUERADE" >> /etc/wireguard/wg0.conf
+echo "PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE; ip6tables -D FORWARD -i wg0 -j ACCEPT; ip6tables -t nat -D POSTROUTING -o eth0 -j MASQUERADE" >> /etc/wireguard/wg0.conf
+echo "SaveConfig = true" >> /etc/wireguard/wg0.conf
