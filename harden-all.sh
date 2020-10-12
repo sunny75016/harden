da98ed162a249fe404618ef436cc466117774f57
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-10-12 07:37:30 +0100

    Update harden-all.sh

--- harden-all.sh
+++ harden-all.sh
@@ -3,18 +3,15 @@
 ./harden-ufw.sh
 ./harden-ppa.sh
 ./harden-kernel.sh
-#./harden-openssl.sh
-#./harden-openssh.sh
+./harden-openssl.sh
+./harden-openssh.sh
 ./harden-profile.sh
 ./harden-chmod.sh
 ./harden-proc.sh
 ./harden-filesystem.sh
-#./harden-install-programs.sh
-./harden-stig.sh
 ./harden-model-files.sh
-#./harden-lynis.sh
 clear
 echo "Thanks for using harden-all.sh from https://github.com/sunny75016/harden.git"
 cd /home/harden > /dev/null
 cat final-checklist
-echo "All done EXCEPT - OpenSSL, OpenSSH, Rkhnter and Lynis"
+echo "All done EXCEPT - Rkhnter and Lynis"
