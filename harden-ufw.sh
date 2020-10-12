074cd9c78b20a31336bed5557b85a7e3fc9e7776
ISO27032 <36335182+sunny75016@users.noreply.github.com>
2020-10-12 07:44:08 +0100

    Update harden-ufw.sh

--- harden-ufw.sh
+++ harden-ufw.sh
@@ -4,7 +4,13 @@ echo "Installing ufw with common ports. Delete rules that are unsuitable."
 apt-get install ufw -y > /dev/null
 ufw allow 22 #SSH > /dev/null
 ufw allow 80 #HTTP  > /dev/null
+ufw allow 110 #HTTP  > /dev/null
+ufw allow 143 #HTTP  > /dev/null
 ufw allow 443 #HTTPS > /dev/null
+ufw allow 465 #HTTP  > /dev/null
+ufw allow 587 #HTTP  > /dev/null
+ufw allow 993  #HTTP  > /dev/null
+ufw allow 995 #HTTP  > /dev/null
 ufw allow 49022 #Example > /dev/null
 ufw enable 
 ufw status
